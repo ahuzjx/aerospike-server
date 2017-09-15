7f8ddd42ed5b0c01f279d48265483fe7484d8f7f
Andrew Gooding <andy@aerospike.com>
2017-09-14 17:26:15 -0700

    AER-5725 - fixed loop that fills family versions to stop at maximum allowed number of families.

--- as/src/fabric/partition_balance.c
+++ as/src/fabric/partition_balance.c
@@ -1423,7 +1423,9 @@ fill_family_versions(const as_partition* p, const sl_ix_t* ns_sl_ix,
 		}
 	}
 
-	for (uint32_t repl_ix = 1; repl_ix < p->n_replicas; repl_ix++) {
+	for (uint32_t repl_ix = 1;
+			repl_ix < p->n_replicas && n_families < AS_PARTITION_N_FAMILIES;
+			repl_ix++) {
 		if (repl_ix == working_master_n) {
 			continue;
 		}
